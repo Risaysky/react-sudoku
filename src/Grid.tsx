3bd31dc14b7ce084bbdba28697f2e5d58903d296
Risaysky <yasserasklou92@gmail.com>
2025-09-12 22:02:28 +0100

    Refactor grid gutter

--- src/Grid.tsx
+++ src/Grid.tsx
@@ -1,5 +1,7 @@
 import type { ReactNode } from "react";
 
+const gutterTemplate = Array.from({ length: 8 });
+
 type GridProps = { children: ReactNode };
 
 export default function Grid({ children }: GridProps) {
@@ -7,71 +9,20 @@ export default function Grid({ children }: GridProps) {
     <>
       <div className="relative grid h-150 w-150 grid-cols-9 grid-rows-9 overflow-hidden rounded-3xl select-none">
         {children}
-        <div
-          className="absolute h-1 w-full bg-slate-300"
-          style={{ top: "calc(1/9 * 100% - .125rem)" }}
-        />
-        <div
-          className="absolute h-1 w-full bg-slate-300"
-          style={{ top: "calc(2/9 * 100% - .125rem)" }}
-        />
-        <div
-          className="absolute z-10 h-1 w-full bg-slate-400"
-          style={{ top: "calc(3/9 * 100% - .125rem)" }}
-        />
-        <div
-          className="absolute h-1 w-full bg-slate-300"
-          style={{ top: "calc(4/9 * 100% - .125rem)" }}
-        />
-        <div
-          className="absolute h-1 w-full bg-slate-300"
-          style={{ top: "calc(5/9 * 100% - .125rem)" }}
-        />
-        <div
-          className="absolute z-10 h-1 w-full bg-slate-400"
-          style={{ top: "calc(6/9 * 100% - .125rem)" }}
-        />
-        <div
-          className="absolute h-1 w-full bg-slate-300"
-          style={{ top: "calc(7/9 * 100% - .125rem)" }}
-        />
-        <div
-          className="absolute h-1 w-full bg-slate-300"
-          style={{ top: "calc(8/9 * 100% - .125rem)" }}
-        />
-
-        <div
-          className="absolute h-full w-1 bg-slate-300"
-          style={{ left: "calc(1/9 * 100% - .125rem)" }}
-        />
-        <div
-          className="absolute h-full w-1 bg-slate-300"
-          style={{ left: "calc(2/9 * 100% - .125rem)" }}
-        />
-        <div
-          className="absolute z-10 h-full w-1 bg-slate-400"
-          style={{ left: "calc(3/9 * 100% - .125rem)" }}
-        />
-        <div
-          className="absolute h-full w-1 bg-slate-300"
-          style={{ left: "calc(4/9 * 100% - .125rem)" }}
-        />
-        <div
-          className="absolute h-full w-1 bg-slate-300"
-          style={{ left: "calc(5/9 * 100% - .125rem)" }}
-        />
-        <div
-          className="absolute z-10 h-full w-1 bg-slate-400"
-          style={{ left: "calc(6/9 * 100% - .125rem)" }}
-        />
-        <div
-          className="absolute h-full w-1 bg-slate-300"
-          style={{ left: "calc(7/9 * 100% - .125rem)" }}
-        />
-        <div
-          className="absolute h-full w-1 bg-slate-300"
-          style={{ left: "calc(8/9 * 100% - .125rem)" }}
-        />
+        {gutterTemplate.map((x, i) => (
+          <div
+            key={i}
+            className={`pointer-events-none absolute h-1 w-full ${(i + 1) % 3 === 0 ? "z-10 bg-slate-400" : "bg-slate-300"}`}
+            style={{ top: `calc(${i + 1}/9 * 100% - .125rem)` }}
+          />
+        ))}
+        {gutterTemplate.map((x, i) => (
+          <div
+            key={i}
+            className={`pointer-events-none absolute h-full w-1 ${(i + 1) % 3 === 0 ? "z-10 bg-slate-400" : "bg-slate-300"}`}
+            style={{ left: `calc(${i + 1}/9 * 100% - .125rem)` }}
+          />
+        ))}
       </div>
     </>
   );
