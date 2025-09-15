b482e5d8593e3ba780f559d6a150c2297ea050c6
Risaysky <yasserasklou92@gmail.com>
2025-09-15 19:11:39 +0100

    Truncate puzzle files

--- src/App.tsx
+++ src/App.tsx
@@ -41,36 +41,66 @@ export default function App() {
   }
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center gap-7 bg-slate-600">
-      <form
-        onSubmit={handleSubmit}
-        className="flex w-150 items-center justify-start gap-3"
-      >
-        <input
-          value={indexInput}
-          onChange={(e) => setIndexInput(e.target.value)}
-          pattern="^\d{1,5}$"
-          required
-          className="h-9 w-25 rounded-2xl bg-slate-50 text-center"
-        />
-        <select
-          className="h-9 w-25 rounded-2xl bg-slate-50 text-center"
-          onChange={(e) => setDifficultyInput(e.target.value as difficultyType)}
+    <>
+      <main className="flex flex-col items-center justify-center gap-7">
+        <form
+          onSubmit={handleSubmit}
+          className="flex w-150 items-center justify-start gap-3"
         >
-          <option value="easy">Easy</option>
-          <option value="medium">Medium</option>
-          <option value="hard">Hard</option>
-          <option value="diabolical">Diabolical</option>
-        </select>
-      </form>
-      <GridLayout>
-        {typeof puzzleIndex === "number" && (
-          <Grid
-            puzzle={getPuzzle(puzzleIndex, puzzleDifficulty)}
-            key={`${puzzleIndex}-${puzzleDifficulty}`}
+          <input
+            value={indexInput}
+            onChange={(e) => setIndexInput(e.target.value)}
+            pattern="^\d{1,5}$"
+            required
+            placeholder="0-99999"
+            className="h-9 w-25 rounded-2xl bg-slate-50 text-center"
           />
-        )}
-      </GridLayout>
-    </main>
+          <select
+            className="h-9 w-25 rounded-2xl bg-slate-50 text-center"
+            onChange={(e) =>
+              setDifficultyInput(e.target.value as difficultyType)
+            }
+          >
+            <option value="easy">Easy</option>
+            <option value="medium">Medium</option>
+            <option value="hard">Hard</option>
+            <option value="diabolical">Diabolical</option>
+          </select>
+          <button className="h-9 w-15 cursor-pointer rounded-2xl bg-sky-200 text-center active:bg-sky-200/70">
+            Go
+          </button>
+        </form>
+        <GridLayout>
+          {typeof puzzleIndex === "number" && (
+            <Grid
+              puzzle={getPuzzle(puzzleIndex, puzzleDifficulty)}
+              key={`${puzzleIndex}-${puzzleDifficulty}`}
+            />
+          )}
+        </GridLayout>
+      </main>
+      <footer className="flex justify-center pb-1 text-slate-100">
+        Puzzles by&nbsp;
+        <a
+          className="flex items-baseline gap-0.5 text-sky-300 hover:underline"
+          href="https://github.com/grantm/sudoku-exchange-puzzle-bank/tree/master"
+          target="_blank"
+        >
+          grantm
+          <svg
+            stroke="currentColor"
+            fill="currentColor"
+            strokeWidth="0"
+            version="1.1"
+            viewBox="0 0 17 17"
+            height=".75rem"
+            width=".75rem"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M15 2v7.5h-1v-5.668l-9.334 9.334-0.707-0.707 9.459-9.459h-5.918v-1h7.5zM11 16h-10v-10h6.574v-1h-7.574v12h12v-7.714h-1v6.714z"></path>
+          </svg>
+        </a>
+      </footer>
+    </>
   );
 }
