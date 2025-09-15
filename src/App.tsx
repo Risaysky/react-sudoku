3f436d9fdc324feb41b6dc8a857181f94623bdd6
Risaysky <yasserasklou92@gmail.com>
2025-09-15 17:56:47 +0100

    Add puzzle selection

--- src/App.tsx
+++ src/App.tsx
@@ -1,139 +1,76 @@
-import { useEffect, useState } from "react";
-import puzzles from "./data/easy.txt?raw";
+import easyPuzzles from "./data/easy.txt?raw";
+import mediumPuzzles from "./data/medium.txt?raw";
+import hardPuzzles from "./data/hard.txt?raw";
+import diabolicalPuzzles from "./data/diabolical.txt?raw";
+import { useState, type FormEvent } from "react";
+import GridLayout from "./GridLayout";
 import Grid from "./Grid";
-import Square from "./Square";
 
-const puzzle = puzzles.split("\n")[2].split(" ")[1].split("");
-const initialConflictedDigits = Array.from({ length: 81 }, () => false);
-const GRID_WIDTH = 9;
-const CELL_WIDTH = 3;
+type difficultyType = "easy" | "medium" | "hard" | "diabolical";
 
-function getCell(index: number) {
-  const cellRow = Math.floor(getRow(index) / CELL_WIDTH);
-  const cellColumn = Math.floor(getColumn(index) / CELL_WIDTH);
-  return `${cellRow}-${cellColumn}`;
-}
+const puzzlesFiles = {
+  easy: easyPuzzles,
+  medium: mediumPuzzles,
+  hard: hardPuzzles,
+  diabolical: diabolicalPuzzles,
+};
 
-function getRow(index: number) {
-  return Math.floor(index / GRID_WIDTH);
-}
-
-function getColumn(index: number) {
-  return index % GRID_WIDTH;
+function getPuzzle(index: number, difficulty: difficultyType) {
+  return puzzlesFiles[difficulty].split("\n")[index].split(" ")[1].split("");
 }
 
 export default function App() {
-  const [solveDigits, setSolveDigits] = useState(puzzle);
-  const [conflictedDigits, setConflictedDigits] = useState(
-    initialConflictedDigits,
-  );
-  const [focusedIndex, setFocusedIndex] = useState<null | number>(null);
+  const [indexInput, setIndexInput] = useState("");
+  const [difficultyInput, setDifficultyInput] =
+    useState<difficultyType>("easy");
+  const [puzzleIndex, setPuzzleIndex] = useState<null | number>(null);
+  const [puzzleDifficulty, setPuzzleDifficulty] =
+    useState<difficultyType>("easy");
 
-  function calcHighlight(digit: string, index: number) {
-    if (index === focusedIndex) return "focus";
-
-    if (focusedIndex === null) return "default";
-
+  function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
     if (
-      solveDigits[focusedIndex] === digit &&
-      focusedIndex !== index &&
-      digit !== "0"
-    )
-      return "same-digit";
-
-    if (
-      getCell(index) === getCell(focusedIndex) ||
-      getColumn(index) === getColumn(focusedIndex) ||
-      getRow(index) === getRow(focusedIndex)
-    )
-      return "geometry";
-
-    return "default";
-  }
-
-  function handleFocus(index: number) {
-    setFocusedIndex(index);
-  }
-
-  useEffect(() => {
-    function handleKeyDown(e: KeyboardEvent) {
-      if (focusedIndex === null) return;
-
-      if (e.key === "ArrowUp" && getRow(focusedIndex) > 0) {
-        setFocusedIndex(focusedIndex - GRID_WIDTH);
-      } else if (
-        e.key === "ArrowDown" &&
-        getRow(focusedIndex) < GRID_WIDTH - 1
-      ) {
-        setFocusedIndex(focusedIndex + GRID_WIDTH);
-      } else if (
-        e.key === "ArrowRight" &&
-        getColumn(focusedIndex) < GRID_WIDTH - 1
-      ) {
-        setFocusedIndex(focusedIndex + 1);
-      } else if (e.key === "ArrowLeft" && getColumn(focusedIndex) > 0) {
-        setFocusedIndex(focusedIndex - 1);
-      }
-
-      if (puzzle[focusedIndex] !== "0") return;
-
-      if (e.key === "Backspace" || e.key === solveDigits[focusedIndex]) {
-        setSolveDigits((solveDigits) => {
-          const temp = [...solveDigits];
-          temp[focusedIndex] = "0";
-          return temp;
-        });
-        return;
-      }
-
-      if (e.key.match(/^[1-9]$/)) {
-        setSolveDigits((solveDigits) => {
-          const temp = [...solveDigits];
-          temp[focusedIndex] = e.key;
-          return temp;
-        });
-        return;
-      }
+      puzzleIndex === null ||
+      ((puzzleIndex !== Number(indexInput) ||
+        puzzleDifficulty !== difficultyInput) &&
+        window.confirm("Leave current puzzle and lose progress?"))
+    ) {
+      setPuzzleIndex(Number(indexInput));
+      setPuzzleDifficulty(difficultyInput);
     }
-    document.addEventListener("keydown", handleKeyDown);
-    return () => document.removeEventListener("keydown", handleKeyDown);
-  }, [focusedIndex, solveDigits]);
-
-  useEffect(() => {
-    const conflictedAcc = [...initialConflictedDigits];
-    for (const [index1, digit1] of solveDigits.entries()) {
-      if (digit1 === "0") continue;
-      const cell1 = getCell(index1);
-      for (const [index2, digit2] of solveDigits.entries()) {
-        if (index1 === index2) continue;
-        if (
-          (cell1 === getCell(index2) ||
-            getColumn(index1) === getColumn(index2) ||
-            getRow(index1) === getRow(index2)) &&
-          digit1 === digit2
-        )
-          conflictedAcc[index1] = true;
-      }
-    }
-    if (JSON.stringify(conflictedAcc) !== JSON.stringify(conflictedDigits))
-      setConflictedDigits(conflictedAcc);
-  }, [conflictedDigits, solveDigits]);
+  }
 
   return (
-    <main className="flex min-h-screen items-center justify-center bg-slate-600">
-      <Grid>
-        {solveDigits.map((d, i) => (
-          <Square
-            key={i}
-            index={i}
-            isPresolved={puzzle[i] !== "0"}
-            isConflicted={conflictedDigits[i]}
-            digit={d}
-            highlight={calcHighlight(d, i)}
-            onFocus={handleFocus}
+    <main className="flex min-h-screen flex-col items-center justify-center gap-7 bg-slate-600">
+      <form
+        onSubmit={handleSubmit}
+        className="flex w-150 items-center justify-start gap-3"
+      >
+        <input
+          value={indexInput}
+          onChange={(e) => setIndexInput(e.target.value)}
+          pattern="^\d{1,5}$"
+          required
+          className="h-9 w-25 rounded-2xl bg-slate-50 text-center"
+        />
+        <select
+          className="h-9 w-25 rounded-2xl bg-slate-50 text-center"
+          onChange={(e) => setDifficultyInput(e.target.value as difficultyType)}
+        >
+          <option value="easy">Easy</option>
+          <option value="medium">Medium</option>
+          <option value="hard">Hard</option>
+          <option value="diabolical">Diabolical</option>
+        </select>
+      </form>
+      <GridLayout>
+        {typeof puzzleIndex === "number" && (
+          <Grid
+            puzzle={getPuzzle(puzzleIndex, puzzleDifficulty)}
+            key={`${puzzleIndex}-${puzzleDifficulty}`}
           />
-        ))}
-      </Grid>
+        )}
+      </GridLayout>
     </main>
   );
 }
--- src/Grid.tsx
+++ src/Grid.tsx
@@ -1,29 +1,137 @@
-import type { ReactNode } from "react";
+import { useEffect, useState } from "react";
+import Square from "./Square";
 
-const gutterTemplate = Array.from({ length: 8 });
+const initialConflictedDigits = Array.from({ length: 81 }, () => false);
+const GRID_WIDTH = 9;
+const CELL_WIDTH = 3;
 
-type GridProps = { children: ReactNode };
+function getCell(index: number) {
+  const cellRow = Math.floor(getRow(index) / CELL_WIDTH);
+  const cellColumn = Math.floor(getColumn(index) / CELL_WIDTH);
+  return `${cellRow}-${cellColumn}`;
+}
 
-export default function Grid({ children }: GridProps) {
+function getRow(index: number) {
+  return Math.floor(index / GRID_WIDTH);
+}
+
+function getColumn(index: number) {
+  return index % GRID_WIDTH;
+}
+
+type GridProps = { puzzle: string[] };
+
+export default function Grid({ puzzle }: GridProps) {
+  const [solveDigits, setSolveDigits] = useState(puzzle);
+  const [conflictedDigits, setConflictedDigits] = useState(
+    initialConflictedDigits,
+  );
+  const [focusedIndex, setFocusedIndex] = useState<null | number>(null);
+
+  function calcHighlight(digit: string, index: number) {
+    if (index === focusedIndex) return "focus";
+
+    if (focusedIndex === null) return "default";
+
+    if (
+      solveDigits[focusedIndex] === digit &&
+      focusedIndex !== index &&
+      digit !== "0"
+    )
+      return "same-digit";
+
+    if (
+      getCell(index) === getCell(focusedIndex) ||
+      getColumn(index) === getColumn(focusedIndex) ||
+      getRow(index) === getRow(focusedIndex)
+    )
+      return "geometry";
+
+    return "default";
+  }
+
+  function handleFocus(index: number) {
+    setFocusedIndex(index);
+  }
+
+  useEffect(() => {
+    function handleKeyDown(e: KeyboardEvent) {
+      if (focusedIndex === null) return;
+
+      if (e.key === "ArrowUp" && getRow(focusedIndex) > 0) {
+        setFocusedIndex(focusedIndex - GRID_WIDTH);
+      } else if (
+        e.key === "ArrowDown" &&
+        getRow(focusedIndex) < GRID_WIDTH - 1
+      ) {
+        setFocusedIndex(focusedIndex + GRID_WIDTH);
+      } else if (
+        e.key === "ArrowRight" &&
+        getColumn(focusedIndex) < GRID_WIDTH - 1
+      ) {
+        setFocusedIndex(focusedIndex + 1);
+      } else if (e.key === "ArrowLeft" && getColumn(focusedIndex) > 0) {
+        setFocusedIndex(focusedIndex - 1);
+      }
+
+      if (puzzle[focusedIndex] !== "0") return;
+
+      if (e.key === "Backspace" || e.key === solveDigits[focusedIndex]) {
+        setSolveDigits((solveDigits) => {
+          const temp = [...solveDigits];
+          temp[focusedIndex] = "0";
+          return temp;
+        });
+        return;
+      }
+
+      if (e.key.match(/^[1-9]$/)) {
+        setSolveDigits((solveDigits) => {
+          const temp = [...solveDigits];
+          temp[focusedIndex] = e.key;
+          return temp;
+        });
+        return;
+      }
+    }
+    document.addEventListener("keydown", handleKeyDown);
+    return () => document.removeEventListener("keydown", handleKeyDown);
+  }, [focusedIndex, puzzle, solveDigits]);
+
+  useEffect(() => {
+    const conflictedAcc = [...initialConflictedDigits];
+    for (const [index1, digit1] of solveDigits.entries()) {
+      if (digit1 === "0") continue;
+      const cell1 = getCell(index1);
+      for (const [index2, digit2] of solveDigits.entries()) {
+        if (index1 === index2) continue;
+        if (
+          (cell1 === getCell(index2) ||
+            getColumn(index1) === getColumn(index2) ||
+            getRow(index1) === getRow(index2)) &&
+          digit1 === digit2
+        )
+          conflictedAcc[index1] = true;
+      }
+    }
+    if (JSON.stringify(conflictedAcc) !== JSON.stringify(conflictedDigits))
+      setConflictedDigits(conflictedAcc);
+  }, [conflictedDigits, solveDigits]);
+
   return (
     <>
-      <div className="relative grid h-150 w-150 grid-cols-9 grid-rows-9 overflow-hidden rounded-3xl bg-slate-50 shadow-md/40 select-none">
-        {children}
-        {gutterTemplate.map((_, i) => (
-          <div
+      {solveDigits !== undefined &&
+        solveDigits.map((d, i) => (
+          <Square
             key={i}
-            className={`pointer-events-none absolute h-0.5 w-full ${(i + 1) % 3 === 0 ? "z-10 bg-slate-400" : "bg-slate-300"}`}
-            style={{ top: `calc(${i + 1}/9 * 100% - .125rem)` }}
+            index={i}
+            isPresolved={puzzle[i] !== "0"}
+            isConflicted={conflictedDigits[i]}
+            digit={d}
+            highlight={calcHighlight(d, i)}
+            onFocus={handleFocus}
           />
         ))}
-        {gutterTemplate.map((_, i) => (
-          <div
-            key={i}
-            className={`pointer-events-none absolute h-full w-0.5 ${(i + 1) % 3 === 0 ? "z-10 bg-slate-400" : "bg-slate-300"}`}
-            style={{ left: `calc(${i + 1}/9 * 100% - .125rem)` }}
-          />
-        ))}
-      </div>
     </>
   );
 }
