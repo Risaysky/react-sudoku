e92ea64aebe15ffa9790645bab0c2a0173d2cf56
Risaysky <yasserasklou92@gmail.com>
2025-09-24 20:59:55 +0100

    Delete unused component

--- src/GridLayout.tsx
+++
@@ -1,9 +0,0 @@
-import type { ReactNode } from "react";
-
-const gutterTemplate = Array.from({ length: 8 });
-
-type GridProps = { children?: ReactNode };
-
-export default function GridLayout({ children }: GridProps) {
-  return <>{children}</>;
-}
